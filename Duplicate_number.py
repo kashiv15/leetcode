4a8761bd7d6db50ba0d4a066bb0ff1b3910eafb1
Kashi Vishwanath <70074984+kashiv15@users.noreply.github.com>
2020-08-31 17:27:51 -0400

    Create Duplicate_number.py

---
+++ Duplicate_number.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in range(len(nums)):
+            if nums.count(nums[i])>1:
+                return nums[i]
